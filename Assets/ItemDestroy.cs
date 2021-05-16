bd946672505198374143de244ae48216f7a7a38e
plusK2021 <81849327+plusK2021@users.noreply.github.com>
2021-05-16 18:41:05 +0900

    Initial commit

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+    //Unityちゃんとアイテムとの距離
+    private float ItemDifference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+        //Unityちゃんとアイテムのz軸方向の距離
+        this.ItemDifference = unitychan.transform.position.z - this.transform.position.z;
+
+        //Unityちゃんが通過してから5mでアイテムを破棄
+        if (ItemDifference >= 5)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+}
